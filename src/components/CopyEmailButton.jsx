03d970ed0d30f5c93caed1dbdb221de06a0e92bf
fesri <fesri1st@gmail.com>
2025-07-30 03:28:42 +0700

    fixing copyng email button

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 const CopyEmailButton = () => {
   const [copied, setCopied] = useState(false);
-  const email = "Your Email Address";
+  const email = "fesri1st@gmail.com";
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(email);
@@ -13,34 +13,15 @@ const CopyEmailButton = () => {
     }, 2000);
   };
   return (
-    <motion.button
-      onClick={copyToClipboard}
-      whileHover={{ y: -5 }}
-      whileTap={{ scale: 1.05 }}
-      className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden"
-    >
+    <motion.button onClick={copyToClipboard} whileHover={{ y: -5 }} whileTap={{ scale: 1.05 }} className="relative px-1 py-4 text-sm text-center rounded-full font-extralight bg-primary w-[12rem] cursor-pointer overflow-hidden">
       <AnimatePresence mode="wait">
         {copied ? (
-          <motion.p
-            className="flex items-center justify-center gap-2"
-            key="copied"
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.1, ease: "easeInOut" }}
-          >
+          <motion.p className="flex items-center justify-center gap-2" key="copied" initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.1, ease: "easeInOut" }}>
             <img src="assets/copy-done.svg" className="w-5" alt="copy Icon" />
             Email has Copied
           </motion.p>
         ) : (
-          <motion.p
-            className="flex items-center justify-center gap-2"
-            key="copy"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.1 }}
-          >
+          <motion.p className="flex items-center justify-center gap-2" key="copy" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.1 }}>
             <img src="assets/copy.svg" className="w-5" alt="copy icon" />
             Copy Email Address
           </motion.p>
